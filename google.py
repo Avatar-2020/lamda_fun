eb897332af6d1eefc9c4b31154c6e1b20d7973b8
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-01-13 05:22:17 -0800

    ver 1.0.7

--- google.py
+++ google.py
@@ -1,4 +1,4 @@
-
+#The latitude of Meerut, Uttar Pradesh, India is 28.984644, and the longitude is 77.705956.
 
 lat,long=input('enter the latitude and longitude with space in decimal form').split()
 try:
